ed2f8b0e80891c1026db34cefc2c4192a3570356
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-05-17 16:49:16 +0200

    [modified :activite_1/js/liensweb.js]

--- activite_1/js/liensweb.js
+++ activite_1/js/liensweb.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Activité 1
 */
 
@@ -25,3 +25,52 @@ var listeLiens = [
 ];
 
 // TODO : compléter ce fichier pour ajouter les liens à la page web
+
+function addLink(masterDiv, link)
+{
+    let section = document.createElement("section"); // creer un element section pour la section
+    let title = document.createElement("h2"); // creer un element h2 pour le titre
+    let titleLink = document.createElement("a"); // creer un element pour le lien du titre
+    let linkS = document.createElement("a"); // creer un element pour le lien a coter
+    let para = document.createElement("p"); // creer un element pour le paragraphe
+
+    /*
+    ** initialise les elements
+    */
+    title.textContent = link.titre;
+    titleLink.href = link.url;
+
+    linkS.href = link.url;
+    linkS.textContent = link.url;
+
+    para.textContent = "Ajouté par : " + link.auteur;
+
+    /*
+    ** initialise le Css
+    */
+    section.style.backgroundColor = "#fff";
+    title.style.color = "#428bca";
+    title.style.textDecoration = "none";
+
+
+    /*
+    ** Ajoute le fils au parents
+    */
+    // ajoute le titre au lien principale
+    titleLink.appendChild(title);
+    // ajoute le titre a la section
+    section.appendChild(titleLink);
+    // ajoute le 2eme lien a la section
+    section.appendChild(linkS);
+    // ajoute le paragraphe a le section
+    section.appendChild(para);
+    // ajoute la section a la div
+    masterDiv.appendChild(section);
+}
+
+let div = document.getElementById("contenu");
+
+for (let i = 0; i < listeLiens.length; i++)
+{
+    addLink(div, listeLiens[i]);
+}
